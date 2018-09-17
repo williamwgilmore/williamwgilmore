9f6e39e3154efbb18808bf67351191556e7d35e3
DESKTOP-SP83B60\willi <williamwgilmore@gmail.com>
2018-09-17 18:04:04 -0500

    Changed em to vh/vw

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,10 +4,14 @@ export default class Welcome extends Component {
 	render() {
 		return (
 			<div id='welcomeBackground'>
-				<div className='text-center centerHeight'>
-					<h1 className='title mainTitle'>William Gilmore</h1>
-					<hr className='divider'/>
-					<h2 className='title subTitle'>Full Stack Web Developer</h2>
+				<div className='row'>
+					<div className='col-md-12'>
+						<div className='text-center centerHeight'>
+							<h1 className='title mainTitle'>William Gilmore</h1>
+							<hr className='divider'/>
+							<h2 className='title subTitle'>Full Stack Web Developer</h2>
+						</div>
+					</div>
 				</div>
 			</div>
 		);
