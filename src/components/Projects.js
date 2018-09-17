a60ac41d0fdd466373d6280713da227f9830c18a
DESKTOP-SP83B60\willi <williamwgilmore@gmail.com>
2018-09-16 22:14:11 -0500

    removed aspire, and moved the projects around

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,40 +11,29 @@ export default class Projects extends Component {
 					</div>
 				</div>
 				<div className='row'>
-					<div className='col-md-4 gameHostSection'>
+					<div className='col-md-4 col-md-offset-2 backgroundZoom gameHostSection'>
 						<h2 className='text-center'>
 							Game Host
 						</h2>
 						<a href='https://radiant-sands-20076.herokuapp.com/breakout' className='gameHostLink' target='_blank' rel="noopener noreferrer" />
-						<p>
+						<p className='experienceText'>
 							For my final project at The University of Texas at Austin's Bootcamp, I created a game hosting site using
 							React, Canvas, Mongoose, and Auth0. The idea behind the site is that the only code that needs to change is
 							the code for different games, while the chatroom or leaderboard list components can be reused for each game.
 						</p>						
 					</div>
-					<div className='col-md-4 immigrationPortalSection'>
+					<div className='col-md-4 backgroundZoom immigrationPortalSection'>
 						<h2 className='text-center'>
 							Immigration Portal
 						</h2>
 						<a href='https://fast-everglades-41667.herokuapp.com/document' className='immigrationPortalLink' target='_blank' rel="noopener noreferrer" />
-						<p>
+						<p className='experienceText'>
 							One of my group projects from UT's coding bootcamp, we found someone looking for a website to help immigrants
 							connect with pro bono lawyers. We never heard back from them, but decided to make a mock website anyways.
 						</p>
 					</div>
-					<div className='col-md-4 aspireSection'>
-						<h2 className='text-center'>
-							<b>Aspire Food Group</b>
-						</h2>
-						<p>
-							A senior developer and I spent a year building an internal tracking website for Aspire Food Group, a 
-							rapidly growing startup. Our system tracked the amount of raw materials consumed and where they were used,
-							and the conditions that produced their product, crickets, most efficiently. 
-							<br /><br />
-							The internal site is confidential, and I cannot share the code or images from the site.
-						</p>
-					</div>
 				</div>
+				<br /><br /><br />
 			</div>
 		);
 	}
