d6a6e0420c210f1ba468e2e42ff7683e44941427
DESKTOP-SP83B60\willi <williamwgilmore@gmail.com>
2018-09-19 10:08:21 -0500

    added a bullet seperator between contact options

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,8 +6,8 @@ export default class NavBar extends Component {
 			<div className='contactBackground'>
 				<div className='row'>
 					<div className='col-md-6 col-md-offset-3 text-center'>
-						<h4 className='contactText'><a href='mailto:williamwgilmore@gmail.com' target='_blank' rel='noopener noreferrer'>Email</a></h4>
-						<h4 className='contactText'><a href='https://www.linkedin.com/in/william-gilmore-90a8a8a4/' target='_blank' rel='noopener noreferrer'>Linkedin</a></h4>
+						<h4 className='contactText'><a href='mailto:williamwgilmore@gmail.com' target='_blank' rel='noopener noreferrer'>Email</a></h4>&bull;
+						<h4 className='contactText'><a href='https://www.linkedin.com/in/william-gilmore-90a8a8a4/' target='_blank' rel='noopener noreferrer'>Linkedin</a></h4>&bull;
 						<h4 className='contactText'><a href='https://github.com/williamwgilmore' target='_blank' rel='noopener noreferrer'>GitHub</a></h4>
 					</div>
 				</div>
